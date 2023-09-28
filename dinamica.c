8c797d0347240477316047c23aa0872aefb0af8b
filipe <143746808+f-lrz@users.noreply.github.com>
2023-09-28 12:18:01 -0300

    Add files via upload

---
+++ dinamica.c
@@ -0,0 +1,29 @@
+#include "listasimpenc.h"
+
+int main(){
+
+    listasimpenc *lista;
+
+    lista = inicializa_listasimpenc();
+
+    insere_listasimpenc_no_fim(&lista, 10);
+    insere_listasimpenc_no_fim(&lista, 20);
+    insere_listasimpenc_no_fim(&lista, 30);
+    insere_listasimpenc_no_fim(&lista, 40);
+    insere_listasimpenc_no_fim(&lista, 50);
+
+    printf("\n");
+
+    imprime_listasimpenc(lista);
+
+    printf("\n");
+
+    remove_listasimpenc(&lista, 30);
+    remove_listasimpenc(&lista, 40);
+
+    imprime_listasimpenc(lista);
+
+    printf("\n");
+
+    return 0;
+}
