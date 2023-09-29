395452e9e163938edbc0cbc2acd4d69c5f141aae
filipe <filipelorenzoarraes@gmail.com>
2023-09-29 10:41:01 -0300

    .

--- dinamica.c
+++ dinamica.c
@@ -1,35 +1,46 @@
 #include "listasimpenc.h"
 
+int insere_listasimpenc_ordenado(listasimpenc **lista, oitem e){
+    listasimpenc *novono, *antatual, *atual;
+
+    novono = aloca_listasimpenc();
+
+    if(novono == NULL) return 0;
+
+    novono->info = e;
+
+    atual = *lista;
+    antatual = NULL;
+
+    while((atual != NULL) && (atual->info < novono->info)){
+        antatual = atual;
+        atual = atual->prox;
+    }
+
+    if(antatual == NULL) *lista = novono;
+    else antatual->prox = novono;
+
+    novono->prox = atual;
+
+    return 1;
+}
+
+
 int main(){
 
     listasimpenc *lista;
 
     lista = inicializa_listasimpenc();
 
-    insere_listasimpenc_no_fim(&lista, 10);
-    insere_listasimpenc_no_fim(&lista, 20);
-    insere_listasimpenc_no_fim(&lista, 30);
-    insere_listasimpenc_no_fim(&lista, 40);
-    insere_listasimpenc_no_fim(&lista, 50);
+    insere_listasimpenc_ordenado(&lista, 10);
+    insere_listasimpenc_ordenado(&lista, 20);
+    insere_listasimpenc_ordenado(&lista, 15);
+    insere_listasimpenc_ordenado(&lista, 30);
 
     printf("\n");
     imprime_listasimpenc(lista);
-
     printf("\n");
-    remove_listasimpenc(&lista, 30);
-    remove_listasimpenc(&lista, 40);
-
-    imprime_listasimpenc(lista);;
-
-    printf("\n%d", tamanho_listasimpenc(lista));
-
-    destroi_listasimpenc(&lista);
-
-    printf("\n");
-    printf("\n%d", tamanho_listasimpenc(lista));
-
-    printf("\n");
-    printf("\n");
+    
 
     return 0;
 }
