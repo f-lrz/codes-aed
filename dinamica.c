a28d709009088c9533442ff21931407999e05cc3
filipe <filipelorenzoarraes@gmail.com>
2023-10-03 12:18:50 -0300

    .

--- dinamica.c
+++ dinamica.c
@@ -25,6 +25,54 @@ int insere_listasimpenc_ordenado(listasimpenc **lista, oitem e){
     return 1;
 }
 
+int no_maior_q_n(listasimpenc **lista, int n){
+    listasimpenc *atual;
+    int cont = 0;
+
+    atual = *lista;
+
+    while(atual != NULL){
+        if(atual->info > n) cont++;
+        atual = atual->prox; 
+    }
+
+
+    return cont;
+}
+
+int listasimpenc_iguais(listasimpenc **l1, listasimpenc **l2){
+    listasimpenc *atual1, *atual2;
+    int tam1 = tamanho_listasimpenc(*l1), tam2 = tamanho_listasimpenc(*l2);
+
+    atual1 = *l1;
+    atual2 = *l2;
+
+    if(tam1 != tam2) return 0;
+
+    while(atual1 != NULL){
+        if(atual1->info != atual2->info) return 0;
+        atual1 = atual1->prox;
+        atual2 = atual2->prox;
+    }
+
+    return 1;
+}
+
+void destroi_impar_listasimpenc(listasimpenc **l){
+    listasimpenc *atual, *antatual;
+
+    atual = *l;
+    antatual = NULL;
+    
+    while(atual != NULL){
+        if(atual->info % 2 == 1){
+            antatual = atual->prox;
+            free(atual);
+        }
+        antatual = atual;
+        atual = atual->prox;
+    }
+}
 
 int main(){
 
@@ -32,12 +80,14 @@ int main(){
 
     lista = inicializa_listasimpenc();
 
-    insere_listasimpenc_ordenado(&lista, 10);
-    insere_listasimpenc_ordenado(&lista, 20);
-    insere_listasimpenc_ordenado(&lista, 15);
-    insere_listasimpenc_ordenado(&lista, 30);
-
+    insere_listasimpenc_no_fim(&lista, 10);
+    insere_listasimpenc_no_fim(&lista, 15);
+    insere_listasimpenc_no_fim(&lista, 20);
+    insere_listasimpenc_no_fim(&lista, 30);
+    insere_listasimpenc_no_fim(&lista, 40);
     printf("\n");
+    imprime_listasimpenc(lista);
+    destroi_impar_listasimpenc(&lista);
     imprime_listasimpenc(lista);
     printf("\n");
     
