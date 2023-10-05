1934c31adde2aa69233ab7fcffb89e581eb1739f
filipe <filipelorenzoarraes@gmail.com>
2023-10-05 11:50:31 -0300

    .

--- dinamica.c
+++ dinamica.c
@@ -1,94 +1,21 @@
 #include "listasimpenc.h"
 
-int insere_listasimpenc_ordenado(listasimpenc **lista, oitem e){
-    listasimpenc *novono, *antatual, *atual;
-
-    novono = aloca_listasimpenc();
-
-    if(novono == NULL) return 0;
-
-    novono->info = e;
-
-    atual = *lista;
-    antatual = NULL;
-
-    while((atual != NULL) && (atual->info < novono->info)){
-        antatual = atual;
-        atual = atual->prox;
-    }
-
-    if(antatual == NULL) *lista = novono;
-    else antatual->prox = novono;
-
-    novono->prox = atual;
-
-    return 1;
-}
-
-int no_maior_q_n(listasimpenc **lista, int n){
-    listasimpenc *atual;
-    int cont = 0;
-
-    atual = *lista;
-
-    while(atual != NULL){
-        if(atual->info > n) cont++;
-        atual = atual->prox; 
-    }
-
-
-    return cont;
-}
-
-int listasimpenc_iguais(listasimpenc **l1, listasimpenc **l2){
-    listasimpenc *atual1, *atual2;
-    int tam1 = tamanho_listasimpenc(*l1), tam2 = tamanho_listasimpenc(*l2);
-
-    atual1 = *l1;
-    atual2 = *l2;
-
-    if(tam1 != tam2) return 0;
-
-    while(atual1 != NULL){
-        if(atual1->info != atual2->info) return 0;
-        atual1 = atual1->prox;
-        atual2 = atual2->prox;
-    }
-
-    return 1;
-}
-
-void destroi_impar_listasimpenc(listasimpenc **l){
-    listasimpenc *atual, *antatual;
-
-    atual = *l;
-    antatual = NULL;
-    
-    while(atual != NULL){
-        if(atual->info % 2 == 1){
-            antatual = atual->prox;
-            free(atual);
-        }
-        antatual = atual;
-        atual = atual->prox;
-    }
-}
-
 int main(){
 
-    listasimpenc *lista;
+    listasimpenc *lista1, *lista2;
 
-    lista = inicializa_listasimpenc();
+    lista1 = inicializa_listasimpenc();
+    lista2 = inicializa_listasimpenc();
 
-    insere_listasimpenc_no_fim(&lista, 10);
-    insere_listasimpenc_no_fim(&lista, 15);
-    insere_listasimpenc_no_fim(&lista, 20);
-    insere_listasimpenc_no_fim(&lista, 30);
-    insere_listasimpenc_no_fim(&lista, 40);
+    insere_listasimpenc_no_fim(&lista1, 10);
+    insere_listasimpenc_no_fim(&lista1, 15);
+    insere_listasimpenc_no_fim(&lista1, 20);
+    insere_listasimpenc_no_fim(&lista2, 30);
+    insere_listasimpenc_no_fim(&lista2, 40);
+    insere_listasimpenc_no_fim(&lista2, 50);
     printf("\n");
-    imprime_listasimpenc(lista);
-    destroi_impar_listasimpenc(&lista);
-    imprime_listasimpenc(lista);
+    conecta_listasimpenc(&lista1, &lista2);
+    imprime_listasimpenc(lista1);
     printf("\n");
     
 
--- listasimpenc.h
+++ listasimpenc.h
@@ -123,5 +123,107 @@ void destroi_listasimpenc(listasimpenc **lista){
     *lista = NULL;
 }
 
+
+
+
+
+
+
+int insere_listasimpenc_ordenado(listasimpenc **lista, oitem e){
+    listasimpenc *novono, *antatual, *atual;
+
+    novono = aloca_listasimpenc();
+
+    if(novono == NULL) return 0;
+
+    novono->info = e;
+
+    atual = *lista;
+    antatual = NULL;
+
+    while((atual != NULL) && (atual->info < novono->info)){
+        antatual = atual;
+        atual = atual->prox;
+    }
+
+    if(antatual == NULL) *lista = novono;
+    else antatual->prox = novono;
+
+    novono->prox = atual;
+
+    return 1;
+}
+
+int no_maior_q_n(listasimpenc **lista, int n){
+    listasimpenc *atual;
+    int cont = 0;
+
+    atual = *lista;
+
+    while(atual != NULL){
+        if(atual->info > n) cont++;
+        atual = atual->prox; 
+    }
+
+
+    return cont;
+}
+
+int listasimpenc_iguais(listasimpenc **l1, listasimpenc **l2){
+    listasimpenc *atual1, *atual2;
+    int tam1 = tamanho_listasimpenc(*l1), tam2 = tamanho_listasimpenc(*l2);
+
+    atual1 = *l1;
+    atual2 = *l2;
+
+    if(tam1 != tam2) return 0;
+
+    while(atual1 != NULL){
+        if(atual1->info != atual2->info) return 0;
+        atual1 = atual1->prox;
+        atual2 = atual2->prox;
+    }
+
+    return 1;
+}
+
+void destroi_impar_listasimpenc(listasimpenc **l){
+    listasimpenc *atual, *antatual;
+
+    atual = *l;
+    antatual = NULL;
+    
+    while(atual != NULL){
+        antatual = atual;
+        atual = atual->prox;
+        if(antatual->info % 2 != 0){
+            remove_listasimpenc(l, antatual->info);
+        }
+    }
+}
+int conecta_listasimpenc(listasimpenc **l1, listasimpenc **l2){
+    listasimpenc *atual;
+
+    atual = *l1;
+
+    while(atual->prox != NULL) atual = atual->prox;
+
+    atual->prox = *l2;
+    
+    atual = *l2;
+
+    while(atual->prox != NULL) atual = atual->prox;
+    atual->prox = *l1;
+
+    *l2 = *l1;
+
+    return 1;
+}
+
+
+
+
+
+
 #endif
 
