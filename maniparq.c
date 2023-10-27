b19f56fc9881b446d2f88d93871c8566003586ad
filipe <filipelorenzoarraes@gmail.com>
2023-10-27 10:31:16 -0300

    .

--- maniparq.c
+++ maniparq.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+int ler_formatado(FILE *arq, char nome[]){
+    float nota1, nota2, nota3;
+
+    arq = fopen(nome, "r");
+    if(!arq) return 0;
+
+    while(fscanf(arq, "%f %f %f", &nota1, &nota2, &nota3) != EOF) printf("Nota 1: %.1f, Nota 2: %.1f, Nota 3: %.1f\n", nota1, nota2, nota3);
+
+    fclose(arq);
+
+    return 1;
+}
+
 int main(){
 
     FILE *arq;
@@ -12,16 +25,16 @@ int main(){
 
     if(!arq) return 0;
 
-    printf("\nMensagem 1:\n");
-    scanf("%[^\n]s", msg);
-    fprintf(arq, strcat(msg, "\n"));
-
-    rewind(arq);
-    fclose(arq);
-    arq = fopen(nome, "r");
-
-    while(fgets(registro, sizeof(registro), arq)) printf("\n%s", registro);
+    fprintf(arq, "9.0 7.0 4.0\n9.0 5.0 5.0\n5.0 7.0 7.0");
 
     fclose(arq);
+
+    ler_formatado(arq, nome);
+
     return 1;
 }
+
+
+/*9.0 7.0 4.0
+  9.0 5.0 5.0
+  5.0 7.0 7.0*/
--- rainha.c
+++ rainha.c
@@ -1,31 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM 4
 
-void inicializa_matriz(int m[][4]){
-	int i, i1;
+void inicializa_matriz(char m[][TAM]){
+	int i, j;
 	for (i = 0; i < 4; i++)
     {
-        for (i1 = 0; i1 < 4; i1++)
+        for (j = 0; j < 4; j++)
         {
-            m[i][i1] = 0;
+            m[i][j] = '#';
         }
     }
 }
-void imprime_matriz(int m[4][4]){
-		printf("         0 1 2 3\n");
-        printf("       0 %d|%d|%d|%d \n", m[0][0], m[0][1], m[0][2], m[0][3]);
-        printf("         ------\n");
-        printf("       1 %d|%d|%d|%d \n", m[1][0], m[1][1], m[1][2], m[1][3]);
-        printf("         ------\n");
-        printf("       2 %d|%d|%d|%d \n", m[2][0], m[2][1], m[2][2], m[2][3]);
-        printf("         ------\n");
-        printf("       3 %d|%d|%d|%d \n", m[3][0], m[3][1], m[3][2], m[3][3]);        
-}
-int tenta(int m[4][4]){
-    
-}
-int main(){
 
-    int tabuleiro[4][4];
+
+void imprime_matriz(char m[TAM][TAM]){
+		printf("         0 1 2 3\n");
+        printf("       0 %c|%c|%c|%c \n", m[0][0], m[0][1], m[0][2], m[0][3]);
+        printf("         ------\n");
+        printf("       1 %c|%c|%c|%c \n", m[1][0], m[1][1], m[1][2], m[1][3]);
+        printf("         ------\n");
+        printf("       2 %c|%c|%c|%c \n", m[2][0], m[2][1], m[2][2], m[2][3]);
+        printf("         ------\n");
+        printf("       3 %c|%c|%c|%c \n", m[3][0], m[3][1], m[3][2], m[3][3]);        
+}
+
+
+int valtab(char m[][TAM], int l, int c){
+    int i, j;
+
+    for(j = 0; j < c; j++){ 
+        if (m[l][j] == 'R') return 0;
+    }
+
+    for(i = l, j = c; i >= 0 && j >= 0; i--, j--){
+        if(m[i][j] == 'R') return 0;
+
+    }
+    for(i = l, j = c; i < TAM && j >= 0; i++, j--){
+        if(m[i][j] == 'R') return 0;
+    }
+
+}
+
+
+int posrainha(char m[TAM][TAM], int c){ 
+    int i;
+
+    if (c>=TAM) return 1;
+       
+    for (i=0; i<TAM; i++){   
+        if(valtab(m, i, c)){
+
+			m[i][c] = 'R';
+			
+    		if(posrainha(m, c+1)) return 1;
+
+ 			else m[i][c] = '#';					
+		}
+	}
+    return 0;	
+}
+
+
+int main(){
+    
+    int col = 0;
+    char tabuleiro[TAM][TAM];
     inicializa_matriz(tabuleiro);
+    posrainha(tabuleiro, col);
     imprime_matriz(tabuleiro);
 }
